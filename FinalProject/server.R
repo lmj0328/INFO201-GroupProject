24be2dd9581390fbb259d59e7f7bbe658cb61cd4
james.griffin <griffj09@uw.edu>
2018-12-03 08:58:34 -0800

    added poverty map

--- FinalProject/server.R
+++ FinalProject/server.R
@@ -5,34 +5,47 @@ library(usmap)
 library(lubridate)
 library(urbnmapr)
 
-
-
-
-
 shinyServer(
   function(input, output) {
     output$yearSalary <- renderPlot({
       RawData <- read.csv(paste0("../Data/SOITaxData/", input$selectYear, ".csv"))
-      
       filteredData <- RawData %>% 
         filter(STATE == "WA") %>%
         filter(COUNTYNAME != "Washington") %>%
         filter(agi_stub == input$selectSalary)
-  
+
       colnames(filteredData)[4] <- "county_name" 
       filteredData$county_name <- as.character(filteredData$county_name)
-      
       joinedData <- filteredData %>%
         left_join(counties, by = "county_name") %>% 
         filter(state_name =="Washington")
-      
       # taxType <- as.factor(paste0("joinedData$", input$selectTax))
-
+      
       ggplot(joinedData, aes(long, lat, group = group, fill = N1)) +
         geom_polygon(color = "#ffffff", size = 0.05) +
         coord_map(projection = "albers", lat0 = 39, lat1 = 45) + 
         labs(fill = "Income tax") + 
         scale_fill_gradientn(colours = terrain.colors(7))
       })
+    output$yearPOV <- renderPlot({
+      wa_poverty_data <- read.csv("../data/PovertyData/wa_poverty_data.csv", stringsAsFactors = FALSE)
+      wa_poverty_data <- wa_poverty_data %>% select(county_name, Percent, Year)
+      wa_county_data <- read.csv("../data/PovertyData/wa_county_data.csv", stringsAsFactors = FALSE)
+      wa_county_data <- wa_county_data %>% select(long, lat, order, group, state_abbv, state_fips, county_name)
+      wa_poverty_final_data <- left_join(wa_county_data, wa_poverty_data, by="county_name")
+      
+      
+      filteredPOVData <- wa_poverty_final_data %>% 
+        filter(Year == input$selectPovYear)
+      
+      ggplot(filteredPOVData, aes(long, lat, group = group, fill = Percent)) +
+        geom_polygon(color = "#ffffff", size = 0.05) +
+        coord_map(projection = "albers", lat0 = 39, lat1 = 45) + 
+        labs(fill = "% Below Poverty Line") + 
+        scale_fill_gradientn(colours = terrain.colors(7))
+    })
+    
+    
   }
+  
 )
