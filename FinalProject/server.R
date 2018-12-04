493551ca81b338d986f22a0d66d795a2bc970871
james.griffin <griffj09@uw.edu>
2018-12-04 00:01:08 -0800

    FinalProject/server.R

--- FinalProject/server.R
+++ FinalProject/server.R
@@ -34,6 +34,15 @@ shinyServer(
       wa_county_data <- wa_county_data %>% select(long, lat, order, group, state_abbv, state_fips, county_name)
       wa_poverty_final_data <- left_join(wa_county_data, wa_poverty_data, by="county_name")
       
+      ditch_the_axes <- theme(
+        axis.text = element_blank(),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank()
+      )
+      
       
       filteredPOVData <- wa_poverty_final_data %>% 
           filter(Year == input$selectPovYear)
@@ -43,12 +52,18 @@ shinyServer(
       ggplot(filteredPOVData, aes(long, lat, group = group, fill = Percent)) +
         geom_polygon(color = "#ffffff", size = 0.05) +
         coord_map(projection = "albers", lat0 = 39, lat1 = 45) + 
-        labs(fill = "% Below Poverty Line") + 
-        scale_fill_gradientn(colours = terrain.colors(7))
+        ggtitle("Washington Poverty Rate For", input$selectPovYear) + 
+        theme(text = element_text(face = "bold")) + 
+        theme(plot.title=element_text(size=32)) +
+        
+        labs(fill = "% Below Poverty Line") + theme_bw() + ditch_the_axes
+        ##scale_fill_gradientn(colours = terrain.colors(7))
+      
     })
-    output$povertyTable <-  DT::renderDataTable({
-      data <- wa_poverty_data[, c("county_name", "Percent", "Year")]
-      DT::datatable(data, options = list(lengthMenu = c(9, 17, 25), pageLength = 9), rownames=FALSE)
+    
+      output$povertyTable <-  DT::renderDataTable({
+        data <- wa_poverty_data[, c("county_name", "Percent", "Year")]
+        DT::datatable(data, options = list(lengthMenu = c(9, 17, 25), pageLength = 9), rownames=FALSE)
     })
     
     
